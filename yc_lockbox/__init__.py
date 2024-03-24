b6ad5adbc81b5c745ee9c7e77a359e55824f452b
akimrx <akimstrong@yandex.ru>
2024-03-24 13:58:02 +0300

    fix(models.Secret): updated refresh() method, the updated attributes are now assigned to the instance of the class

--- yc_lockbox/__init__.py
+++ yc_lockbox/__init__.py
@@ -27,7 +27,7 @@ SOFTWARE.
 from yc_lockbox._lockbox import YandexLockboxClient
 from yc_lockbox._models import Secret, INewSecretPayloadEntry, INewSecret, INewSecretVersion, IUpdateSecret
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Akim Faskhutdinov"
 __author_email__ = "akimstrong@yandex.ru"
 __license__ = "MIT"
